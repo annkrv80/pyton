6e76b584751dd13df40c22a6cef43583e53d0099
Anna <annkrv80@yandex.ru>
2022-09-25 21:50:48 +0700

    Управляющая конструкция If else

---
+++ HelloPython/if.py
@@ -0,0 +1,16 @@
+a = int(input('Введите первое число:'))
+b = int(input('Введите второе число:'))
+if a>b:
+    print(a)
+else:
+    print(b)
+
+username = input('Введите имя:')
+if username == 'Маша':
+    print('Ура,это же Маша')
+elif username == 'Марина':
+    print('Я так долшл ждал Вас,Марина')
+elif username == 'Ильнар':
+    print('Ильнар - топ')
+else:
+    print('Привет,', username)
