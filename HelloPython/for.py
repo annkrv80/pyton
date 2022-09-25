1f79e99e89093871751593e9d72919bf867274d6
Anna <annkrv80@yandex.ru>
2022-09-25 22:33:39 +0700

    Управляющая конструкция for

---
+++ HelloPython/for.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,10,5]
+for i in list:
+    print(i**2)
+#####################
+r = range(10)
+for i in r:
+    print(i)
+####################
+for i in range(5):
+    print(i)
+      
+#Если требеутся другой интервал и шаг не еденица
+for i in range(10,20,3):
+    print(i)      
+
+#Можно разбить строку
+for i in 'mama':
+    print(i)
