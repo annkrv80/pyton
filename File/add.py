16d41d9d74069748331f47073c3d117ce8b35a95
Anna <annkrv80@yandex.ru>
2022-10-03 00:06:02 +0700

    Добавление записи в файл

--- File/add.py
+++ File/add.py
@@ -1,4 +1,12 @@
-colors = ['red','green','blue']
-data = open('file.txt','a')
-data.writelines(colors)
-data.close()
+#colors = ['red','green','blue']
+#data = open('file.txt','a')
+#data.writelines(colors)
+#data.write('\nLine\n')
+#data.write('Line3\n')
+#data.close()
+  
+
+with open('file.txt','a') as data:
+    data.write('line11\n')
+    data.write('line22\n')
+
