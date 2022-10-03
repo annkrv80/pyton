ff221024eaf0556b7b68dd4a7d746327098e0e81
Anna <annkrv80@yandex.ru>
2022-10-04 00:08:45 +0700

    Словари

---
+++ Function/slovari.py
@@ -0,0 +1,25 @@
+dictionary = {}
+dictionary = \
+ {
+ 'up': '↑',
+ 'left': '←',
+ 'down': '↓',
+ 'right': '→'
+ }
+print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+print(dictionary['left']) # ←
+# типы ключей могут отличаться
+#Неупорядоченные коллекции произвольных
+#объектов с доступом по ключу
+#Словари
+print(dictionary['up']) # ↑
+# типы ключей могут отличаться
+dictionary['left'] = '⇐'
+print(dictionary['left']) # ⇐
+#print(dictionary['type']) # KeyError: 'type'
+del dictionary['left'] # удаление элемента
+for item in dictionary: # for (k,v) in dictionary.items():
+ print('{}: {}'.format(item, dictionary[item]))
+# up: ↑
+# down: ↓
+# right: →
